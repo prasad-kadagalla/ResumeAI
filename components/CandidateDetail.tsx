29527bda603959b3f4f5503fdbdc3464f316e844
prasad-kadagalla <kadagallap@gmail.com>
2025-12-14 10:09:47 +0530

    Delete components directory

--- components/CandidateDetail.tsx
+++
@@ -1,8 +0,0 @@
-export default function CandidateDetail() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h2>Candidate Detail</h2>
-      <p>Placeholder CandidateDetail component.</p>
-    </div>
-  );
-}
